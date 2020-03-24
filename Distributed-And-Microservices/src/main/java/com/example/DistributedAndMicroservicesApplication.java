91716c94f8c5ad3fbbd2f7469cc0e71949b1d2b7
Ronald-Fi <327554567@qq.com>
2020-03-24 17:17:14 +0800

    update readme

---
+++ Distributed-And-Microservices/src/main/java/com/example/DistributedAndMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAndMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedAndMicroservicesApplication.class, args);
+	}
+
+}
