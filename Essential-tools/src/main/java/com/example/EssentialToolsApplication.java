8236a56a2395da96b471aec932acbd6d9f40476b
Ronald-Fi <327554567@qq.com>
2020-03-24 17:13:48 +0800

    add module

---
+++ Essential-tools/src/main/java/com/example/EssentialToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialToolsApplication.class, args);
+	}
+
+}
